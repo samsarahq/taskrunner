69cba9f63d302996a4600912d818e60a2a012b3b
Tonia Hsia <tonia.hsia@samsara.com>
2023-01-24 12:49:49 -0600

    taskrunner task: update Task type to support flags
    We would like to support passing flags to a task via CLI
    (e.g. taskrunner task/name --flag, taskrunner task/name --key="val")
    
    Update the Task type to include a Flags field which allows
    devs to specify a list of supported flags on the task definition.
    
    Every specified flag has the following fields:
    * Description - a description of the flag's effect
    * LongName - the multi-char flag name
    * ShortName - the single-char flag name (specified by rune)
    * Default - the default value of the flag
    * ValueType - the type of value the flag specifies (bool/int/string)
    
    We expect flags to specify string, bool, int, float64 and
    duration types, so we have provided the following enum for ValueType:
    * StringTypeFlag
    * BoolTypeFlag
    * IntTypeFlag
    * Float64TypeFlag
    * DurationTypeFlag
    
    Eventually, we will pass the flags passed to the task via CLI args
    into the RunWithOptions fn as a third argument of type map[string]FlagArg
    which will include helper fns to cast the string CLI arg into its
    correct type (depending on ValueType).
    
    The FlagArg includes the following fields:
    * Value - raw value collected from os.Args
    * StringVal - helper fn that returns ptr to string val for string flags
    * BoolVal - helper fn that returns ptr to bool val for bool flags
    * IntVal - helper fn that returns ptr to int val for int flags
    * Float64Val - helper fn that returns ptr to float64 val for float64 flags
    * DurationVal - helper fn that returns ptr to time.Duration for duration flags

--- task.go
+++ task.go
@@ -2,6 +2,7 @@ package taskrunner
 
 import (
 	"context"
+	"time"
 
 	"github.com/samsarahq/taskrunner/shell"
 )
@@ -15,9 +16,19 @@ type Task struct {
 	// XXX: Pass files in so that task can decide to do less work, i.e. if
 	// you change a go file we can run go fmt on just that file.
 
+	// [DEPRECATED] This fn is deprecated in favor of RunWithFlags
 	// Run is called when the task should be run. The function should gracefully
 	// handle context cancelation.
 	Run func(ctx context.Context, shellRun shell.ShellRun) error
+
+	// RunWithFlags is called when the task should be run
+	// and includes all flags passed to the task. The function
+	// should gracefully handle context cancellation.
+	RunWithFlags func(ctx context.Context, shellRun shell.ShellRun, flagArgs map[string]FlagArg) error
+
+	// Flags is a list of supported flags for this task (e.g. --var="val", -bool).
+	// Only the flags specified in this list will be considered valid arguments to a task.
+	Flags []TaskFlag
 
 	ShouldInvalidate func(event InvalidationEvent) bool
 
@@ -42,4 +53,47 @@ type Task struct {
 	// IsGroup specifies whether this task is a pseudo-task that groups
 	// other tasks underneath it.
 	IsGroup bool
+}
+
+type TaskFlag struct {
+	// Description should be a string that describes what effect passing the flag to a task has.
+	// This description is shown automatically in the generated --help message.
+	// This field must be non-empty.
+	Description string
+
+	// ShortName is the single character version of the flag. If not 0, the
+	// option flag can be 'activated' using -<ShortName>. Either ShortName
+	// or LongName needs to be non-empty.
+	ShortName rune
+
+	// LongName is the multi-character version of the flag. If not "", the flag can be
+	// activated using --<LongName>. Either ShortName or LongName needs to be non-empty.
+	LongName string
+
+	// Default is the default value for the flag.
+	Default string
+
+	// ValueType describes what the flag type is (e.g. --flag [ValueType])
+	// and can be one of either: StringTypeFlag, BoolTypeFlag,
+	// IntTypeFlag, Float64TypeFlag, or DurationTypeFlag.
+	// Its value is shown automatically in the generated --help message.
+	// This field must be non-empty.
+	ValueType string
+}
+
+const (
+	StringTypeFlag   string = "string"
+	BoolTypeFlag     string = "bool"
+	IntTypeFlag      string = "int"
+	Float64TypeFlag  string = "float64"
+	DurationTypeFlag string = "duration"
+)
+
+type FlagArg struct {
+	Value       interface{}
+	StringVal   func() *string
+	BoolVal     func() *bool
+	IntVal      func() *int
+	Float64Val  func() *float64
+	DurationVal func() *time.Duration
 }
