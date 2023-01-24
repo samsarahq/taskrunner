83cd13f1b3b12df8edbf0871191316bd8dd2fa6c
Tonia Hsia <tonia.hsia@samsara.com>
2023-01-24 12:49:49 -0600

    taskrunner task: update Task type to support options
    We would like to support passing options to a task via CLI
    (e.g. taskrunner task/name --option)
    
    Update the Task type to include an Options field which allows
    devs to specify a list of supported options on the task definition.
    
    Every specified option has the following fields:
    * Description - a description of the option's effect
    * LongName - the multi-char option name
    * ShortName - the single-char option name (specified by rune)
    * Default - the default value of the option
    * ValueType - the type of value the option specifies (bool/int/string)
    
    Since we only expect options to specify bool/int/string types,
    we have provided the following enum for ValueType:
    * OptionBoolType
    * OptionStringType
    * OptionIntType
    
    Eventually, we will pass the options passed to the task as CLI args
    into the RunWithOptions fn as a third argument of type map[string]OptionArg.
    
    The OptionArg includes the following fields:
    * Value - raw value collected from os.Args
    * BoolVal - helper fn that returns bool val for bool options
    * IntVal - helper fn that returns int val for int options
    * StringVal - helper fn that returns sting val for string options

--- task.go
+++ task.go
@@ -15,9 +15,19 @@ type Task struct {
 	// XXX: Pass files in so that task can decide to do less work, i.e. if
 	// you change a go file we can run go fmt on just that file.
 
+	// [DEPRECATED] This fn is deprecated in favor of RunWithFlags
 	// Run is called when the task should be run. The function should gracefully
 	// handle context cancelation.
 	Run func(ctx context.Context, shellRun shell.ShellRun) error
+
+	// RunWithFlags is called when the task should be run
+	// and includes all flags passed to the task. The function
+	// should gracefully handle context cancellation.
+	RunWithFlags func(ctx context.Context, shellRun shell.ShellRun, flagArgs map[string]FlagArg) error
+
+	// Flags is a list of supported flags for this task (e.g. --var="val", -bool).
+	// Only the flags specified in this list will be considered valid arguments to a task.
+	Flags []TaskFlag
 
 	ShouldInvalidate func(event InvalidationEvent) bool
 
@@ -42,4 +52,39 @@ type Task struct {
 	// IsGroup specifies whether this task is a pseudo-task that groups
 	// other tasks underneath it.
 	IsGroup bool
+}
+
+type TaskFlag struct {
+	// Description should be a string that describes what effect passing the flag to a task has.
+	// This description is shown automatically in the generated --help message.
+	Description string
+
+	// ShortName is the single character version of the flag. If not 0, the
+	// option flag can be 'activated' using -<ShortName>. Either ShortName
+	// or LongName needs to be non-empty.
+	ShortName rune
+
+	// LongName is the multi-character version of the flag. If not "", the flag can be
+	// activated using --<LongName>. Either ShortName or LongName needs to be non-empty.
+	LongName string
+
+	// Default is the value of the flag. Note that bool flags (--bool) are automatically defaulted
+	// to true if passed as a supported flag.
+	Default string
+
+	// ValueType describes what the flag type is (e.g. --flag [ValueName]).
+	ValueType string
+}
+
+const (
+	StringTypeFlag string = "string"
+	IntTypeFlag    string = "int"
+	BoolTypeFlag   string = "bool"
+)
+
+type FlagArg struct {
+	Value     interface{}
+	BoolVal   func() bool
+	IntVal    func() int
+	StringVal func() string
 }
