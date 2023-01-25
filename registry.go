0c0a009997f34beced61c251147b63d858b69377
Tonia Hsia <tonia.hsia@samsara.com>
2023-01-25 12:07:09 -0600

    taskrunner registry: add validation to passed task options
    When adding a task to the task registry, validate that all options
    specified in the task Options field:
    * Have either a LongName or ShortName specified
    * Do not use the reserved "help"/"h" LongName and ShortName options
    * Include a Description
    * Include a ValueType that matches the OptionString/Bool/OptionIntType enum vals
    
    Additionally, ensure that no duplicate LongName or ShortName options exist
    for the same task.

--- registry.go
+++ registry.go
@@ -13,12 +13,14 @@ var DefaultRegistry = NewRegistry()
 func NewRegistry() *Registry {
 	return &Registry{
 		definitions: make(map[string]*Task),
+		flagsByTask: make(map[string]map[string]TaskFlag),
 	}
 }
 
 // Registry is an object that keeps track of task definitions.
 type Registry struct {
 	definitions map[string]*Task
+	flagsByTask map[string]map[string]TaskFlag
 }
 
 type TaskOption func(*Task) *Task
@@ -32,6 +34,59 @@ func (r *Registry) Add(t *Task, opts ...TaskOption) *Task {
 		panic(fmt.Sprintf("Duplicate task registered: %s", t.Name))
 	}
 	r.definitions[t.Name] = t
+
+	r.flagsByTask[t.Name] = map[string]TaskFlag{}
+	for _, flag := range t.Flags {
+		// Validate that the following task TaskFlag fields are provided:
+		// - LongName or ShortName
+		// - Disallow --help/-h flags
+		// - Description
+		// - ValueType
+		if flag.LongName == "" && flag.ShortName == 0 {
+			panic(fmt.Sprintf("Neither LongName nor ShortName are defined for an flag on %s. At least one name must be defined.", t.Name))
+		}
+
+		if flag.LongName != "" && flag.LongName == "help" {
+			panic("The long flag name `help` is reserved.")
+		}
+
+		if flag.ShortName != 0 && string(flag.ShortName) == "h" {
+			panic("The short lag name `h` is reserved.")
+		}
+
+		if flag.Description == "" {
+			var optionName string
+			if flag.LongName != "" {
+				optionName = flag.LongName
+			} else {
+				optionName = string(flag.ShortName)
+			}
+			panic(fmt.Sprintf("Please provide a description for the `%s` flag", optionName))
+		}
+
+		if flag.ValueType != StringTypeFlag && flag.ValueType != BoolTypeFlag && flag.ValueType != IntTypeFlag {
+			panic("Please set the flag ValueType with `taskrunner.StringTypeFlag`, `taskrunner.BoolTypeFlag` or `taskrunner.IntTypeFlag`.")
+		}
+
+		// Validate that there are no duplicate LongNames
+		if flag.LongName != "" {
+			_, ok := r.flagsByTask[t.Name][flag.LongName]
+			if ok {
+				panic(fmt.Sprintf("Duplicate flag LongName registered: %s", flag.LongName))
+			}
+			r.flagsByTask[t.Name][flag.LongName] = flag
+		}
+
+		// Validate that there are no duplicate ShortNames
+		if flag.ShortName != 0 {
+			_, ok := r.flagsByTask[t.Name][string(flag.ShortName)]
+			if ok {
+				panic(fmt.Sprintf("Duplicate flag ShortName registered: %s", string(flag.ShortName)))
+			}
+			r.flagsByTask[t.Name][string(flag.ShortName)] = flag
+		}
+	}
+
 	return t
 }
 
